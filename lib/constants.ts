bd4af415b95ea7aa2578100ab4cbbc6ad8c00430
manjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2021-10-17 11:18:06 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,17 +14,17 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://zumba.vercel.app/';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const BRAND_NAME = 'Hubli Zumba Club';
+export const SITE_NAME_MULTILINE = ['Hubli', 'Zumba Club'];
+export const SITE_NAME = 'Hubli Zumba Club';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'This is an open hubli zumba club to join and dance together';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
+  'This is an open hubli zumba club to join and dance together';
+export const DATE = 'October 17, 2021';
 export const SHORT_DATE = 'Oct 27 - 9:00am PST';
 export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
